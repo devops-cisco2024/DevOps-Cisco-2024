2aaa19df5fa5e6494b067b3e6c201d933cea4f20
Vadym Pavlenko <pavvadymq@gmail.com>
2024-04-29 19:50:37 +0300

    Ready to merge

--- sftp_setup.sh
+++ sftp_setup.sh
@@ -12,18 +12,10 @@ PASSWD=$(date | md5sum | cut -c1-8)
 echo "sftpuser:$PASSWD" | chpasswd
 # Setting up Chroot
 mkdir -p /srv/sftpuser/.ssh
-cat /home/vagrant/.ssh/id_rsa.pub >> /srv/sftpuser/.ssh/authorized_keys
 chown root /srv/sftpuser
 chmod g+rx /srv/sftpuser
 mkdir -p /srv/sftpuser/data/trace
 chown -hR sftpuser:sftpuser /srv/sftpuser/data
-
-
-# Setting up rkhunter, log would be saved in /var/log/rkhunter.log files
-apt-get install --yes rkhunter --no-install-recommends
-rkhunter --update
-rkhunter --propupd
-rkhunter --checkall --skip-keypress --createlogfile || true
 
 # Restarting sshd server to be able to connect via sftp without errors
 systemctl restart sshd
